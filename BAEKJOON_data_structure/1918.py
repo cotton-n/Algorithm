e934a54f3094b70f207edaad0b64b85f2f725d66
yeji <yeji9175@naver.com>
2020-05-14 02:53:29 +0900

    [UPDATE]

--- BAEKJOON_data_structure/1918.py
+++ BAEKJOON_data_structure/1918.py
@@ -3,6 +3,40 @@
 """
 import sys
 
-string = sys.stdin.readline().rstrip()
+
+def solution(st):
+    answer = ''
+
+    op = ''
+    bop = ''  # 괄호 안 연산
+    flag = 0
+    bflag = 0  # 괄호안에있는것
+    stack = []
+    for s in st:
+        if s == '(':
+            bflag = 1
+            print(s)
+        elif s == ')':
+            bflag = 0
+            print(s)
+        elif bflag == 1:
+            # 괄호 안 일때
+            print(s)
+        elif s == '+' or s == '-' or s == '*' or s == '/':
+            op = s
+            flag = 1
+        else:
+            if flag == 1:
+                stack.append(s)
+                stack.append(op)
+                flag = 0
+            else:
+                stack.append(s)
+    answer = ''.join(stack)
+    return answer
 
 
+if __name__ == '__main__':
+    string = sys.stdin.readline().rstrip()
+    result = solution(string)
+    print(result)
