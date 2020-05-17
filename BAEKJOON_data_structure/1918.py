9b7615ed56acefe14d3e01bf80d20f27a4579c32
yeji <yeji9175@naver.com>
2020-05-18 03:14:10 +0900

    [UPDATE]

--- BAEKJOON_data_structure/1918.py
+++ BAEKJOON_data_structure/1918.py
@@ -1,72 +1,32 @@
 """
-후위 표기식
+후위 표기식 - 검색
 """
+from sys import stdin, stdout
+sysIn = stdin.readline()
+sysOut = stdout.write
 
-# TODO: (a+b-c)*d*e 괄호 바로 오른쪽에 *이나 /가 있을 때 반례 생김
-# TODO: 저 괄호 안에 있는 우선순위도 생각해줘야함
-import sys
-
-
-def cal(stack, o, f, n):
-    flag = f
-    op = o
-    if n == '+' or n == '-' or n == '*' or n == '/':
-        op.append(n)
-        flag = 1
-    else:
-        if flag == 1:
-            stack.append(n)
-            if op:
-                stack.append(op.pop())
-            flag = 0
-        else:
-            stack.append(n)
-    return [op, flag]
+priority = {'*': 2, '/': 2, '+': 1, '-': 1, '(': 0}
 
 
-def solution(st):
-    print(st)
+def solve(string):
     op = []
-    b_op = []  # 괄호안 연산
-    flag = 0
-    b = 0  # 괄호안인지 아닌지
-    p = 0  # 우선순위
-    stack = []
-    b_stack = []
-    for s in st:
-        if s == '(':
-            b = 1
-            p += 1
+    for s in string:
+        if 'A' <= s <= 'Z':
+            sysOut(s)
+        elif s == '(':
+            op.append(s)
         elif s == ')':
-            while b_op:
-                b_stack.append(b_op.pop())
-            p -= 1
-            if p == 0:
-                stack.append(''.join(b_stack))
-                if op:
-                    stack.append(op.pop())
-                b_stack = []
-                b = 0
-        elif b == 1:
-            # 괄호안 일때
-            b_op, b_flag = cal(b_stack, b_op, 0, s)
+            while op and op[-1] != '(':
+                sysOut(op.pop())
+            op.pop()
         else:
-            op, flag = cal(stack, op, flag, s)
-    answer = ''.join(stack)
-    return answer
+            while op and priority[s] <= priority[op[-1]]:
+                sysOut(op.pop())
+            op.append(s)
+    while op:
+        sysOut(op.pop())
+    sysOut('\n')
 
 
 if __name__ == '__main__':
-    string = sys.stdin.readline().rstrip()
-    size = len(string)
-    string_stack = []
-    i = 0
-    while i < size:
-        if (string[i] == '*' or string[i] == '/') and string[i+1] != '(':
-            string_stack.append('('+string_stack.pop()+string[i]+string[i+1]+')')
-            i += 2
-        else:
-            string_stack.append(string[i])
-            i += 1
-    result = solution(''.join(string_stack))
-    print(result)
+    solve(sysIn.rstrip())
