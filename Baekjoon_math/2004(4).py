4263438d0d0d6e5960d554cb53d0383f02812634
yeji <yeji9175@naver.com>
2020-05-28 04:09:59 +0900

    [UPDATE]

--- Baekjoon_math/2004(4).py
+++ Baekjoon_math/2004(4).py
@@ -7,54 +7,24 @@ import sys
 sysIn = sys.stdin.readline()
 
 
-# 2와 5의 개수를 얻는 함수
-def count_twoFive(n):
-    two = 0
-    five = 0
-    while n % 2 == 0 or n % 5 == 0:
-        if n % 2 == 0:
-            n = n / 2
-            two += 1
-        if n % 5 == 0:
-            n = n / 5
-            five += 1
-    return [two, five]
-
-
-def factorial_twoFive(n, r):
-    two, five = 0, 0
+def solution(nums):
+    a, b = map(int, nums.split())
     two1, five1 = 0, 0
     two2, five2 = 0, 0
     two3, five3 = 0, 0
-    for i in range(2, n+1):
-        t, f = count_twoFive(i)
+
+    for i in range(2, a + 1):
+        t, f = i // 2, i // 5
         two1 += t
         five1 += f
-        if i <= r:
+        if i <= b:
             two2 += t
             five2 += f
-        if i <= n-r:
+        if i <= a - b:
             two3 += t
             five3 += f
-    two = two1 - (two2 + two3)
-    five = five1 - (five2 + five3)
-    return [two, five]
 
-
-def solution(nums):
-    answer = 0
-    a, b = map(int, nums.split())
-
-    two, five = factorial_twoFive(a, b)
-    if two == 0 or five == 0:
-        return 0
-    else:
-        while two != 0 and five != 0:
-            two -= 1
-            five -= 1
-            answer += 1
-
-    return answer
+    return min(two1 - (two2 + two3), five1 - (five2 + five3))
 
 
 if __name__ == '__main__':
