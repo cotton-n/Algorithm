77fd702afb5517e20fb628f2bc5bbb8514fab56c
yeji <yeji9175@naver.com>
2020-05-27 22:03:42 +0900

    [UPDATE]

--- Baekjoon_math/2004.py
+++ Baekjoon_math/2004.py
@@ -2,16 +2,40 @@
 조합 0의 개수
 """
 from sys import stdin, stdout
+
 sysIn = stdin.readline()
 sysOut = stdout.write
 
-#  TODO: nCm 계산하는 함수 만들기
+
+def factorial(x):
+    if x <= 1:
+        return 1
+    else:
+        return x * factorial(x - 1)
 
 
-def solve(n):
-    a, b = map(int, n.split())
-    print(a, b)
+def permutation(n, r):
+    return factorial(n)/factorial(n-r)
+
+
+def combination(n, r):
+    return permutation(n, r)/factorial(r)
+
+
+def solution(nums):
+    a, b = map(int, nums.split())
+    comb = str(int(combination(a, b)))
+
+    answer = 0
+
+    for c in comb:
+        if c == '0':
+            answer += 1
+        else:
+            answer = 0
+
+    return answer
 
 
 if __name__ == '__main__':
-    solve(sysIn.rstrip())
+    print(solution(sysIn.rstrip()))
